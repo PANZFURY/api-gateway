39be48caa941d887d3efe785e2cbde05f4d68ac3
Mikhail <mikhailveqluw@gmail.com>
2025-12-18 09:25:40 +0300

    add jwt filter

---
+++ src/main/java/practical/task/gateway/filter/JwtGatewayFilter.java
@@ -0,0 +1,68 @@
+package practical.task.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public class JwtGatewayFilter implements GlobalFilter, Ordered {
+
+    private static final List<String> PUBLIC_PATHS = List.of(
+            "/auth/login",
+            "/auth/refresh"
+    );
+
+    private final WebClient authClient;
+
+    public JwtGatewayFilter(WebClient.Builder builder) {
+        this.authClient = builder
+                .baseUrl("http://auth-service:8081")
+                .build();
+    }
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().value();
+
+
+        if (PUBLIC_PATHS.stream().anyMatch(path::startsWith)) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return authClient.post()
+                .uri("/auth/validate")
+                .header(HttpHeaders.AUTHORIZATION, authHeader)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .then(chain.filter(exchange))
+                .onErrorResume(ex -> {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                });
+    }
+
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
